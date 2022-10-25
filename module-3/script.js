76b665fc2cbdf1c77841213705d5d8dafa50ac08
GRACESTER <106128101+GRACESTER@users.noreply.github.com>
2022-10-25 11:26:22 -0500

    Add files via upload

---
+++ module-3/script.js
@@ -0,0 +1,24 @@
+/* Steranko, Assignment 3, 25 October 2022*/
+function firstFunction(){
+    let sentenceOne = "The nature of my work makes it very hard to work from home. I mostly translated birth certificates for new recruits in Omaha.";
+
+    document.getElementById('one').innerHTML = sentenceOne
+}
+
+function secondFunction(){
+    let sentenceTwo = "I was disappointed that now I wouldn't be able to focus on school as much as others would be able to. ";
+
+    document.getElementById('two').innerHTML = sentenceTwo
+}
+
+function thirdFunction(){
+    let sentenceThree = "I scrambled to get my uniform put together and also realized that I may need some t-shirts in a larger size.";
+
+    document.getElementById('three').innerHTML = sentenceThree
+}
+
+function fourthFunction(){
+    let sentenceFour = "There was also a few new faces that I hadn't seen before.";
+
+    document.getElementById('four').innerHTML = sentenceFour
+}
